98b1d40441955c6fb2946b7c9ffa2cc24a8eb346
ross lazarus <ross.lazarus@gmail.com>
2017-06-08 16:56:58 +1000

    Adding new stuff - need to swap the R code file names - shortly

---
+++ drive_kmMay2017.R
@@ -0,0 +1,187 @@
+# for data from https://www.backblaze.com/hard-drive-test-data.html
+# ross lazarus me fecit february 18 2016
+# ggsurv modified from
+# http://www.r-statistics.com/2013/07/creating-good-looking-survival-curves-the-ggsurv-function/
+# the legend now corresponds to the order of the very last survival point so
+# it's easier to see which is which...
+
+library(survival)
+library(rms)
+
+setwd('/data/drivefail')
+
+ggsurv <- function(s, CI = 'def', plot.cens = T, surv.col = 'gg.def',
+                   cens.col = 'red', lty.est = 1, lty.ci = 2,
+                   cens.shape = 3, back.white = T, xlab = 'Time',
+                   ylab = 'Survival', main = ''){
+ 
+  library(ggplot2)
+  strata <- ifelse(is.null(s$strata) ==T, 1, length(s$strata))
+  stopifnot(length(surv.col) == 1 | length(surv.col) == strata)
+  stopifnot(length(lty.est) == 1 | length(lty.est) == strata)
+ 
+  ggsurv.s <- function(s, CI = 'def', plot.cens = T, surv.col = 'gg.def',
+                       cens.col = 'red', lty.est = 1, lty.ci = 2,
+                       cens.shape = 3, back.white = F, xlab = 'Time',
+                       ylab = 'Survival', main = ''){
+ 
+    dat <- data.frame(time = c(0, s$time),
+                      surv = c(1, s$surv),
+                      up = c(1, s$upper),
+                      low = c(1, s$lower),
+                      cens = c(0, s$n.censor))
+    dat.cens <- subset(dat, cens != 0)
+ 
+    col <- ifelse(surv.col == 'gg.def', 'black', surv.col)
+ 
+    pl <- ggplot(dat, aes(x = time, y = surv)) +
+      xlab(xlab) + ylab(ylab) + ggtitle(main) +
+      geom_step(col = col, lty = lty.est)
+ 
+    pl <- if(CI == T | CI == 'def') {
+      pl + geom_step(aes(y = up), color = col, lty = lty.ci) +
+        geom_step(aes(y = low), color = col, lty = lty.ci)
+    } else (pl)
+ 
+    pl <- if(plot.cens == T & length(dat.cens) > 0){
+      pl + geom_point(data = dat.cens, aes(y = surv), shape = cens.shape,
+                       col = cens.col)
+    } else if (plot.cens == T & length(dat.cens) == 0){
+      stop ('There are no censored observations')
+    } else(pl)
+ 
+    pl <- if(back.white == T) {pl + theme_bw()
+    } else (pl)
+    pl
+  }
+ 
+  ggsurv.m <- function(s, CI = 'def', plot.cens = T, surv.col = 'gg.def',
+                       cens.col = 'red', lty.est = 1, lty.ci = 2,
+                       cens.shape = 3, back.white = T, xlab = 'Time',
+                       ylab = 'Survival', main = '') {
+    n <- s$strata
+ 
+    ugroups <- unlist(strsplit(names(s$strata), '='))[seq(2, 2*strata, by = 2)]
+    getlast = function(x) {
+      res=NULL
+      for (mo in names(x$strata)) {
+         sur = x[mo]$surv
+         n = length(sur)
+         res = append(res,sur[n])
+         }
+      return(res)
+      }
+    lastv = ugroups[order(getlast(s),decreasing=T)]
+    groups = factor(ugroups,levels = lastv)
+    gr.name <-  unlist(strsplit(names(s$strata), '='))[1]
+    gr.df <- vector('list', strata)
+    ind <- vector('list', strata)
+    n.ind <- c(0,n); n.ind <- cumsum(n.ind)
+    for(i in 1:strata) ind[[i]] <- (n.ind[i]+1):n.ind[i+1]
+ 
+    for(i in 1:strata){
+      gr.df[[i]] <- data.frame(
+        time = c(0, s$time[ ind[[i]] ]),
+        surv = c(1, s$surv[ ind[[i]] ]),
+        up = c(1, s$upper[ ind[[i]] ]),
+        low = c(1, s$lower[ ind[[i]] ]),
+        cens = c(0, s$n.censor[ ind[[i]] ]),
+        group = rep(groups[i], n[i] + 1))
+    }
+ 
+    dat <- do.call(rbind, gr.df)
+    dat.cens <- subset(dat, cens != 0)
+ 
+    pl <- ggplot(dat, aes(x = time, y = surv, group = group)) +
+      xlab(xlab) + ylab(ylab) + ggtitle(main) +
+      geom_step(aes(col = group, lty = group))
+ 
+    col <- if(length(surv.col == 1)){
+      scale_colour_manual(name = gr.name, values = rep(surv.col, strata))
+    } else{
+      scale_colour_manual(name = gr.name, values = surv.col)
+    }
+ 
+    pl <- if(surv.col[1] != 'gg.def'){
+      pl + col
+    } else {pl + scale_colour_discrete(name = gr.name)}
+ 
+    line <- if(length(lty.est) == 1){
+      scale_linetype_manual(name = gr.name, values = rep(lty.est, strata))
+    } else {scale_linetype_manual(name = gr.name, values = lty.est)}
+ 
+    pl <- pl + line
+ 
+    pl <- if(CI == T) {
+      if(length(surv.col) > 1 && length(lty.est) > 1){
+        stop('Either surv.col or lty.est should be of length 1 in order
+             to plot 95% CI with multiple strata')
+      }else if((length(surv.col) > 1 | surv.col == 'gg.def')[1]){
+        pl + geom_step(aes(y = up, color = group), lty = lty.ci) +
+          geom_step(aes(y = low, color = group), lty = lty.ci)
+      } else{pl +  geom_step(aes(y = up, lty = group), col = surv.col) +
+               geom_step(aes(y = low,lty = group), col = surv.col)}
+    } else {pl}
+ 
+ 
+    pl <- if(plot.cens == T & length(dat.cens) > 0){
+      pl + geom_point(data = dat.cens, aes(y = surv), shape = cens.shape,
+                      col = cens.col)
+    } else if (plot.cens == T & length(dat.cens) == 0){
+      stop ('There are no censored observations')
+    } else(pl)
+ 
+    pl <- if(back.white == T) {pl + theme_bw()
+    } else (pl)
+    pl
+  }
+  pl <- if(strata == 1) {ggsurv.s(s, CI , plot.cens, surv.col ,
+                                  cens.col, lty.est, lty.ci,
+                                  cens.shape, back.white, xlab,
+                                  ylab, main)
+  } else {ggsurv.m(s, CI, plot.cens, surv.col ,
+                   cens.col, lty.est, lty.ci,
+                   cens.shape, back.white, xlab,
+                   ylab, main)}
+  pl
+}
+
+fn = 'drivefail_resMay2017'
+f = paste(fn,'xls',sep='.')
+d = read.delim(f,sep='\t',head=T)
+
+titl = 'KM curves from Backblaze drive data to Q1 2017'
+tm = table(d$manufact)
+ds = subset(d,tm[d$manufact]>200)
+s = with(ds,Surv(time=obsdays,event=status))
+km.manu = npsurv(s ~ manufact, data=ds)
+ofnpdf = paste(fn,'_manufacturer.pdf',sep='')
+ofnpng = paste(fn,'_manufacturer.png',sep='')
+ofnsvg = paste(fn,'_manufacturer.svg',sep='')
+svg(ofnsvg)
+ggsurv(km.manu,main = titl)
+dev.off()
+png(ofnpng,height=1000,width=1600)
+ggsurv(km.manu,main = titl)
+dev.off()
+pdf(ofnpdf,height=10,width=20)
+ggsurv(km.manu,main = titl)
+dev.off()
+tmo = table(d$model)
+dm = subset(d,tmo[d$model] > 500)
+sm = with(dm,Surv(time=obsdays,event=status))
+km.mod = npsurv(sm ~ model, data=dm)
+ofnpdf = paste(fn,'_model.pdf',sep='')
+ofnpng = paste(fn,'_model.png',sep='')
+ofnsvg = paste(fn,'_model.svg',sep='')
+svg(ofnsvg)
+ggsurv(km.mod,main = titl)
+dev.off()
+png(ofnpng,height=1000,width=1600)
+ggsurv(km.mod,main = titl)
+dev.off()
+pdf(ofnpdf,height=10,width=20)
+ggsurv(km.mod,main = titl)
+dev.off()
+survdiff(sm ~ model, data=dm, rho=0)
+survdiff(s ~ manufact, data=ds, rho=0)
