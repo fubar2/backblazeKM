80bfbae8ee7e93bccfc3bce3b93448971b115c7f
fubar2 <ross.lazarus@gmail.com>
2018-07-28 12:28:33 +1000

    restoring after stuff up again

--- drive_km.R
+++ drive_km.R
@@ -43,49 +43,48 @@ setwd('/data/drivefail')
 options(width=512)
 runtag = 'july6_no_resurrection'
 fn = paste('drivefail_res',runtag,sep='')
-titl = 'KM plots - Backblaze drive data (hours) to end Q1 2018'
+titl = 'KM plots - Backblaze drive data to end Q1 2018'
 subtitl = paste('Run on',runtag,sep=' ')
 # changeme!!!
-runtag = paste(runtag,'hours',sep='_')
+
 
 f = paste(fn, 'xls', sep = '.')
 d = read.delim(f, sep = '\t', head = T)
-sillyhours = (d$smarthours > 80000)
-sh = paste(d$smarthours[sillyhours],collapse=',')
-print(paste('found >100000 hour smart9 records = ',sh,sep=''))
-d$smarthours[sillyhours] = 80000 # 
 tm = table(d$manufact)
 ds = subset(d, tm[d$manufact] > 200)
-s = with(ds, Surv(time = smarthours, event = status))
+#toobig = (ds$serial == 'MJ0351YNGA723A') 
+#ds$obsdays[toobig] = 1610 # most are censored around this time up to q1 2018 MJ0351YNGA723A
+# doesn't help - so many censored about the same time...
+s = with(ds, Surv(time = obsdays, event = status))
 km.manu = npsurv(s ~ manufact, data = ds)
-ofnpdf = paste(fn, '_hours_manufacturer.pdf', sep = '')
-ofnpng = paste(fn, '_hours_manufacturer.png', sep = '')
-ofnsvg = paste(fn, '_hours_manufacturer.svg', sep = '')
+ofnpdf = paste(fn, '_manufacturer.pdf', sep = '')
+ofnpng = paste(fn, '_manufacturer.png', sep = '')
+ofnsvg = paste(fn, '_manufacturer.svg', sep = '')
+svg(ofnsvg)
+ggsurv(km.manu, main = titl)
+dev.off()
 png(ofnpng, height = 1000, width = 1600)
-ggsurv(km.manu, main = titl, CI = F, lty.ci = 2, size.ci = 0.1)
+ggsurv(km.manu, main = titl)
 dev.off()
 pdf(ofnpdf, height = 10, width = 20)
-ggsurv(km.manu, main = titl,  CI = F, lty.ci = 2, size.ci = 0.1)
-dev.off()
-svg(ofnsvg)
-ggsurv(km.manu, main = titl, CI = F, lty.ci = 2, size.ci = 0.1)
+ggsurv(km.manu, main = titl)
 dev.off()
 tmo = table(d$model)
 dm = subset(d, tmo[d$model] > 200)
 # dm = subset(dm,)
-sm = with(dm, Surv(time = smart9hours, event = status))
+sm = with(dm, Surv(time = obsdays, event = status))
 km.mod = npsurv(sm ~ model, data = dm)
-ofnpdf = paste(fn, '_hours_model.pdf', sep = '')
-ofnpng = paste(fn, '_hours_model.png', sep = '')
-ofnsvg = paste(fn, '_hours_model.svg', sep = '')
+ofnpdf = paste(fn, '_model.pdf', sep = '')
+ofnpng = paste(fn, '_model.png', sep = '')
+ofnsvg = paste(fn, '_model.svg', sep = '')
+svg(ofnsvg)
+ggsurv(km.mod, main = titl)
+dev.off()
 png(ofnpng, height = 1000, width = 1600)
-ggsurv(km.mod, main = titl,  CI = F, lty.ci = 2, size.ci = 0.1)
+ggsurv(km.mod, main = titl)
 dev.off()
 pdf(ofnpdf, height = 10, width = 20)
-ggsurv(km.mod, main = titl,  CI = F, lty.ci = 2, size.ci = 0.1)
-dev.off()
-svg(ofnsvg)
-ggsurv(km.mod, main = titl,  CI = F, lty.ci = 2, size.ci = 0.1)
+ggsurv(km.mod, main = titl)
 dev.off()
 survdiff(sm ~ model, data = dm, rho = 0)
 survdiff(s ~ manufact, data = ds, rho = 0)
@@ -96,7 +95,22 @@ survdiff(s ~ manufact, data = ds, rho = 0)
 # so try only modelling the first week and so on
 # some interesting urban myths about early vs late failures?
 #
-cutps = c(72, 1440, 2880, 10000, 3000, 10000, 20000, 30000, 40000)
+# eeesh - big data = big data problems
+# one HDS5C3030ALA630 has a fail at 1801 days but most drives are censored at about 1600 days
+# makes plot misleading...
+#
+# smart9 hours also broken - 9269 of 14345 values exceed 10 years worth of days!!
+# as at end Q1 2018
+#> max(dm$obsdays)
+#[1] 1816
+#> max(dm$smart9hours,na.rm=T)
+#[1] 274169
+#> 1816*24 # total possible hours
+#[1] 43584
+# > sum(dt$smart9hours > 43600)
+#[1] 9296
+
+cutps = c(3, 7, 15, 30, 60, 90, 120, 360, 720, 1080, 1440, 1802)
 
 ncut = length(cutps)
 for (i in c(1:ncut))
@@ -104,33 +118,34 @@ for (i in c(1:ncut))
   nmax = cutps[i]
   titl = paste('KM first',
                nmax,
-               'hours of observation curves from Backblaze drive data to Q1 2018')
+               'days of observation curves from Backblaze drive data to Q1 2017')
   dti = dm
-  fixme = (dti$smart9hours > nmax) # ignore all data, failing or not beyond nmax, by censoring at nmax.
+  fixme = (dti$obsdays > nmax) # ignore all data, failing or not beyond nmax, by censoring at nmax.
+  dti$status[fixme] = 0
+  dti$obsdays[fixme] = nmax
   okmodels = subset(dti,fixme,select=c(model))
   models = levels(factor(as.character(okmodels$model)))
   modcol = models[order(models)]
   if (i == 1) {
     mdf = data.frame('model' = modcol)
   }
-  dti$status[fixme] = 0
-  dti$smart9hours[fixme] = nmax
+  
   inmodels = (dti$model %in% models)
   dt = subset(dti, inmodels)
-  stm = with(dt, Surv(time = smart9hours, event = status))
+  stm = with(dt, Surv(time = obsdays, event = status))
   km.mod = npsurv(stm ~ model, data = dt)
   kmmod = survdiff(stm ~ model, data = dt, rho = 0)
   pres = fixres(kmmod)
   rnk = data.frame(rank=pres[order(pres$groups), ]$rank)
   mdf = cbindX(mdf, rnk)# cbind(mdf, rnk)
-  s = paste('*** KM statistics for first',nmax,'SMART hours')
+  s = paste('*** KM statistics for first',nmax,'days')
   print.noquote(s)
   print(pres)
   ofnroot = paste('km_first',
-                 nmax,
-                 '_hours_model_',
-                 runtag,
-                 sep = '')
+                  nmax,
+                  '_days_model_',
+                  runtag,
+                  sep = '')
   ofnpdf = paste(ofnroot,'.pdf',sep = '')
   ofnpng = paste(ofnroot,'.png',sep = '')
   png(ofnpng, height = 1000, width = 1600)
@@ -138,24 +153,14 @@ for (i in c(1:ncut))
   ## ggplot requires explicit printing inside loops
   dev.off()
   pdf(ofnpdf, height = 10, width = 20)
-  print(ggsurv(km.mod, main = titl,  CI = F, lty.ci = 2, size.ci = 0.1))
+  print(ggsurv(km.mod, main = titl))
   dev.off()
 }
 
-colnames(mdf) = c('Model', paste('Rank_hour', cutps, sep = '_'))
+colnames(mdf) = c('Model', paste('Rank_day', cutps, sep = '_'))
 mdfs = mdf[, 2:ncut]
 vmdf = apply(mdfs, 1, var,na.rm=TRUE)
 mmdf = apply(mdfs, 1, mean,na.rm=TRUE)
 mdfo = cbind(mdf, 'mean' = mmdf, 'var' = vmdf)
 mdfo = mdfo[order(mdfo$mean), ]
 print(mdfo)
-
-# some piccys of the awful smart data
-lm = d$manufact
-plot(d$obsdays,d$smarthours,col=unclass(lm)+1,cex=0.1)
-legend(1550,80000, legend=levels(lm),col=(1:length(levels(lm))),  pch=19, cex=0.6)
-lm = d$model
-plot(d$obsdays,d$smarthours,col=unclass(lm)+1,cex=0.1)
-legend(1550,80000, legend=levels(lm),col=(1:length(levels(lm))),  pch=19, cex=0.4)
-
-
